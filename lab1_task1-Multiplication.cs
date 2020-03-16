d56a83ddfd8271e901c04e925e3689ea90ccce3c
EldarLis <eldaarliis@gmail.com>
2020-03-16 16:12:11 +0300

    Add files via upload

---
+++ lab1_task1-Multiplication.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.WriteLine("Input x and y");
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+
+            z = Multiplication(x, y);
+
+            Console.WriteLine("Multi: {0}", z);
+        }
+
+        static int Multiplication(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ lab1_task3-FunToZero.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 1, b = 2, eps;
+            //a = Convert.ToDouble(Console.ReadLine());
+            //b = Convert.ToDouble(Console.ReadLine());
+            eps = Convert.ToDouble(Console.ReadLine());
+
+            double x, xz;
+
+            if (f(a) * SecondDerivative(a) > 0)
+                xz = a;
+            else xz = b;
+
+            x = xz;
+
+            while (Math.Abs(f(x)) > eps)
+            {
+                x -= f(x) / FirstDerivative(xz);
+
+            }
+
+            Console.WriteLine("f(x) = {0:##.###########}", f(x));
+        }
+
+        static double f(double x)
+        {
+            return (Math.Cos(2 / x) - 2 * Math.Sin(1 / x) + 1 / x);
+        }
+
+        static double FirstDerivative(double x)
+        {
+            return (2 * (Math.Sin(2 / x) + Math.Cos(1 / x) + 1)) / (x * x);
+        }
+
+        static double SecondDerivative(double x)
+        {
+            return -((4 * Math.Sin(2 / x) + 4 * Math.Cos(1 / x) + 4) * x + 4 * Math.Cos(2 / x) - 2 * Math.Sin(1 / x)) / (x * x * x * x); 
+        }
+
+    }
+}
